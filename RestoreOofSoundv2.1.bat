7709f8f6a396a8dcf517fb47a10b2771777524a9
carriethekid <62395331+akidinatophat@users.noreply.github.com>
2022-08-29 15:59:19 -0400

    Add files via upload

--- RestoreOofSoundv2.1.bat
+++ RestoreOofSoundv2.1.bat
@@ -1,5 +1,5 @@
 @echo off
-title RestoreOofSound v2.0
+title RestoreOofSound v2.1
 echo Getting Roblox Version...
 powershell "$json = Invoke-WebRequest https://clientsettings.roblox.com/v2/client-version/WindowsPlayer; $verupload = ConvertFrom-Json $json; $verupload.clientVersionUpload | Out-File C:\Users\$env:UserName\tempcurrentverupload.txt"
 cd C:\Users\%USERNAME%
@@ -9,7 +9,7 @@ SET CurrentVersionUpload=%%F
 if exist "C:\Users\%USERNAME%\AppData\Local\Roblox\Versions\%CurrentVersionUpload%\RobloxPlayerLauncher.exe" (
 echo Changing death sound...
 del C:\Users\%USERNAME%\AppData\Local\Roblox\Versions\%CurrentVersionUpload%\content\sounds\ouch.ogg
-curl -s https://github.com/akidinatophat/RestoreOofSound/raw/main/ouch.ogg --output C:\Users\%USERNAME%\AppData\Local\Roblox\Versions\%CurrentVersionUpload%\content\sounds\ouch.ogg
+curl -s -L https://github.com/akidinatophat/RestoreOofSound/raw/main/ouch.ogg --output C:\Users\%USERNAME%\AppData\Local\Roblox\Versions\%CurrentVersionUpload%\content\sounds\ouch.ogg
 color 2
 echo Success!
 del C:\Users\%USERNAME%\tempcurrentverupload.txt
